c10441284be879be9def934f9d5a0e73d923546d
Admiral-Simo <admiralsimo16@gmail.com>
2025-03-03 16:17:51 +0000

    Error handling

--- src/main/java/com/atlas/tourguide/mappers/CategoryMapper.java
+++ src/main/java/com/atlas/tourguide/mappers/CategoryMapper.java
@@ -2,10 +2,12 @@ package com.atlas.tourguide.mappers;
 
 import java.util.List;
 
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.stereotype.Repository;
 
 import com.atlas.tourguide.domain.PostStatus;
 import com.atlas.tourguide.domain.dtos.CategoryDto;
@@ -13,6 +15,7 @@ import com.atlas.tourguide.domain.dtos.CreateCategoryRequest;
 import com.atlas.tourguide.domain.entities.Category;
 import com.atlas.tourguide.domain.entities.Post;
 
+@Repository
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CategoryMapper {
 	@Mapping(target = "postCount", source = "posts", qualifiedByName = "calculatePostCount")
